168844c5450f3980328071ce9874afb713a2d0bb
James Cole <james@openstack.org>
2021-02-11 12:57:56 -0600

    Add theme info

--- catalog/index.js
+++ catalog/index.js
@@ -5,12 +5,26 @@ import { Catalog, pageLoader } from "catalog";
 const pages = [
   {
     path: "/",
-    title: "OpenInfra Brand Guide",
+    title: "Open Infrastructure Foundation",
     content: pageLoader(() => import("./WELCOME.md"))
   }
 ];
 
 ReactDOM.render(
-  <Catalog title="Catalog" pages={pages} />,
+  <Catalog title="Brand Guidelines" 
+  pages={pages} 
+  theme={{
+      textColor: "#000102",
+      background: "#f6f6f6",
+      linkColor: "#EC1944",
+      pageHeadingBackground: "#EC1944",
+      pageHeadingTextColor: "#fff",
+      navBarBackground: "#EEEEEE",
+      navBarTextColor: "#133e68",
+      fontFamily: "'Open Sans', sans-serif",
+      fontHeading: "'Open Sans bold', sans-serif",
+      fontMono: "'Source Code Pro', monospace"
+    }} 
+  />,
   document.getElementById("catalog")
 );
